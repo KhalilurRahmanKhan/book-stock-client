039006b4d11d9561b7fe3591b203aee956cc4a39
Khalilur Rahman Khan <khanjahid373@gmail.com>
2022-05-19 10:32:21 +0600

    navbar add

---
+++ src/pages/Home/Home.jsx
@@ -0,0 +1,12 @@
+import Navbar from '../../components/Navbar/Navbar';
+import './home.css';
+
+function Home() {
+  return (
+    <div className="home">
+    <Navbar></Navbar>
+    </div>
+  );
+}
+
+export default Home;
